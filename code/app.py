a6406faceb52f5b593ac803e2d756bcb5bec9a67
pathadeTush <pathadetc19.comp@coep.ac.in>
2021-10-18 21:00:18 +0530

    fixed navbar links

---
+++ code/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', title='Home')
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='About')
+
+@app.route('/register')
+def register():
+    return render_template('register.html', title='Register')
+
+@app.route('/login')
+def login():
+    return render_template('login.html', title='Login')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
