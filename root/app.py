9ddcd4942b0d5ce21754ae249297450e487d1d23
pathadeTush <pathadetc19.comp@coep.ac.in>
2021-10-22 22:10:54 +0530

    Fixed import error

--- root/app.py
+++ root/app.py
@@ -1,28 +1,27 @@
 from flask import Flask, render_template, url_for
 
-app = Flask(__name__)
+main_app = Flask(__name__)
 
-@app.route('/')
-@app.route('/home')
+@main_app.route('/')
+@main_app.route('/home')
 def home():
     return render_template('home.html', title='Home')
 
-@app.route('/about')
+@main_app.route('/about')
 def about():
     return render_template('about.html', title='About')
 
-@app.route('/register', methods=['GET', 'POST'])
+@main_app.route('/register', methods=['GET', 'POST'])
 def register():
     return render_template('register.html', title='Register')
 
-@app.route('/login', methods=['GET', 'POST'])
+@main_app.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template('login.html', title='Login')
 
-@app.route('/account', methods=['GET', 'POST'])
+@main_app.route('/account', methods=['GET', 'POST'])
 def account():
     return render_template('account.html', title='Account')
 
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    main_app.run(debug=True)
